e447b5d3e74f67dc795f29cf4020526753bfc404
Klevtsov <sergei.klevtsov@accenture.com>
2018-09-09 21:01:45 +0300

    Start using props in folded components

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,32 @@
 import React from 'react'
 
+function Toolbar(props) {
+  // The Toolbar component must take an extra "theme" prop
+  // and pass it to the ThemedButton. This can become painful
+  // if every single button in the app needs to know the theme
+  // because it would have to be passed through all components.
+  return (
+    <div>
+      <ThemedButton theme={props.theme} />
+    </div>
+  );
+}
+
+function ThemedButton(props) {
+  return <Button theme={props.theme} />;
+}
+
+function Button(props) {
+  return <button data-theme={props.theme}>Push me</button>
+}
+
 const App = () => {
-  return <h3>Hello World!</h3>
+  return (
+    <React.Fragment>
+      <h3>Hello World!</h3>
+      <Toolbar theme="dark" />
+    </React.Fragment>
+  )
 }
 
 export default App
