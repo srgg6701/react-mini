22603038493b6d4586f544ea6fc9f9e4487d37a0
Klevtsov <sergei.klevtsov@accenture.com>
2018-09-09 21:12:40 +0300

    Alternative implementation

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,8 @@
 import React from 'react'
 
-function Toolbar(props) {
-  // The Toolbar component must take an extra "theme" prop
-  // and pass it to the ThemedButton. This can become painful
-  // if every single button in the app needs to know the theme
-  // because it would have to be passed through all components.
-  return (
-    <div>
-      <ThemedButton theme={props.theme} />
-    </div>
-  );
-}
-
-function ThemedButton(props) {
-  return <Button theme={props.theme} />;
-}
-
-function Button(props) {
-  return <button data-theme={props.theme}>Push me</button>
-}
+const Toolbar = props => (<HandleBar cl="ultraviolet" />)
+const HandleBar = props => (<Button classNm={props.cl} />)
+const Button = props => (<button className={props.classNm}>Btn</button>)
 
 const App = () => {
   return (
