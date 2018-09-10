f9f139c4dcbabf818bb8156c68b668342f4bd963
Klevtsov <sergei.klevtsov@accenture.com>
2018-09-10 10:24:15 +0300

    Experimeting, cleaning code, adding comments

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,10 @@ import React from 'react'
 const Toolbar = () => {
   return (<HandleBar />)
 }
-const HandleBar = ({props}) => {
+const HandleBar = () => {
   return (
     <ThemeContext.Consumer>
-      {props => <Button {...props}></Button>}
+      {someprops => <Button {...someprops}></Button>}
     </ThemeContext.Consumer>
   )
 }
@@ -14,7 +14,6 @@ const Button = ( props ) => {
   console.log(props);
   return (<button className={props.class_name}>{props.text}</button>)
 }
-/* */
 
 const value = { class_name: "dark", text: "Push me" };
 const ThemeContext = React.createContext(value);
@@ -23,6 +22,9 @@ const App = () => {
   return (
     <React.Fragment>
       <h3>Hello World!</h3>
+      <p>Look, we don't need to pass <em>props</em> as an argument down the hierarchy anymore!</p>
+      <p>Moreover, no matter which argument name we pass to <b>HandleBar</b> children.</p>
+      <hr />
       <ThemeContext.Provider value={value}>
         <Toolbar />
       </ThemeContext.Provider>
