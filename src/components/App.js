58632b84e3fd08a9c9d08a48e67f63ac9b571005
Klevtsov <sergei.klevtsov@accenture.com>
2018-09-14 19:41:54 +0300

    Moving contents to modules

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,6 @@
 import React from 'react'
-
-const Toolbar = () => {
-  return (<HandleBar />)
-}
-const HandleBar = () => {
-  return (
-    <ThemeContext.Consumer>
-      { // the name of arguments don't matter because it considers Provider data 
-        someprops => <Button {...someprops}></Button>
-      }
-    </ThemeContext.Consumer>
-  )
-}
-const Button = ( props ) => {
-  return (<button className={props.class_name}>{props.text}</button>)
-}
-
-const value = { class_name: "dark", text: "Push me" };
-const ThemeContext = React.createContext(value);
+import './debug'
+import {Toolbar, value, ThemeContext} from './Theme'
 
 const App = () => {
   return (
---
+++ src/components/Theme.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Toolbar = () => {
+    return <HandleBar />
+}
+const HandleBar = () => {
+    return (
+        <ThemeContext.Consumer>
+            { // the name of arguments don't matter because it considers Provider data 
+                someprops => <Button {...someprops}></Button>
+            }
+        </ThemeContext.Consumer>
+    )
+}
+const Button = (props) => {
+    return (<button className={props.class_name}>{props.text}</button>)
+}
+
+const value = { class_name: "dark", text: "Push me" };
+const ThemeContext = React.createContext(value);
+
+export {Toolbar, value, ThemeContext}
