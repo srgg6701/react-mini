9f81fa6c72e11cea8fb91218db95ab670fd9e919
Klevtsov <sergei.klevtsov@accenture.com>
2018-09-15 11:39:47 +0300

    Fixing a routing problem, got another

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import React from 'react'
 import './components/debug'
-import {Toolbar, value, ThemeContext} from './components/Theme'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
+import { Toolbar, value, ThemeContext } from './components/Theme'
 import Home from './components/Home'
+import Default from './components/Default'
+
+console.log({ BrowserRouter, Route, Link, Default });
 
 const App = () => {
   return (
     <React.Fragment>
-      <h3>Hello World!</h3>
-      <p>Look, we don't need to pass <em>props</em> as an argument down the hierarchy anymore!</p>
-      <p>Moreover, no matter which argument name we pass to <b>HandleBar</b> children.</p>
-      <hr />
-      <ThemeContext.Provider value={value}>
-        <Toolbar />
-      </ThemeContext.Provider>
-      <hr/>
-      <Home />
+      <ul>
+        <li>
+          <Link to="/home">Home</Link>
+        </li>
+        <li>
+          <Link to="/default">Default</Link>
+        </li>
+      </ul>
+      <h1>Hello, Dude!</h1>
+      <Route path='/home' exact component={Home} />
+      <Route path='/default' component={Default} />
     </React.Fragment>
   )
 }
