2eacd38140a9d066db9b75e8471a0370f0a4c24c
Klevtsov <sergei.klevtsov@accenture.com>
2018-09-15 15:20:01 +0300

    Setting Server-Side-Rendering!

--- server/middleware/renderer.js
+++ server/middleware/renderer.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOMServer from 'react-dom/server'
 
 // import our main App component
-import App from '../../src/App';
+import App from '../../src/components/App';
 
 const path = require("path");
 const fs = require("fs");
